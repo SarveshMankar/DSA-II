d72f6054620ae0c6d03f72f7b63babb9dc4bbb70
Sam-Tech-2543 <mankarsarvesh2543@gmail.com>
2023-04-28 13:36:32 +0530

    Test Cases

--- Codes/AVL/main.c
+++ Codes/AVL/main.c
@@ -30,20 +30,28 @@ void main(){
     // insert(&t, 15);
     // insert(&t, 5);
 
-    insert(&t, 178);
-    insert(&t, 32);
-    insert(&t, 59);
-    insert(&t, 205);
-    insert(&t, 210);
-    insert(&t, 80);
-    insert(&t, 75);
-    insert(&t, 66);
-    insert(&t, 100);
-    insert(&t, 125);
+    // insert(&t, 178);
+    // insert(&t, 32);
+    // insert(&t, 59);
+    // insert(&t, 205);
+    // insert(&t, 210);
+    // insert(&t, 80);
+    // insert(&t, 75);
+    // insert(&t, 66);
+    // insert(&t, 100);
+    // insert(&t, 125);
+    // insert(&t, 10);
+    // insert(&t, 15);
+    // insert(&t, 108);
+    // insert(&t, 84);
+
     insert(&t, 10);
-    insert(&t, 15);
-    insert(&t, 108);
-    insert(&t, 84);
+    insert(&t, 20);
+    insert(&t, 30);
+    insert(&t, 40);
+    insert(&t, 50);
+    insert(&t, 25);
+
 
     preorder(t);
     printf("-----------------\n");
